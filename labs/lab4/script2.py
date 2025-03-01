55d1eeacf82693a71c3c0bafd035038fa28b61a2
Sia P <byx7ex@virginia.edu>
2025-02-28 19:42:54 -0500

    Add python script

---
+++ labs/lab4/script2.py
@@ -0,0 +1,44 @@
+import boto3
+import requests
+import os
+
+#Client for reusable connection
+s3 = boto3.client('s3', region_name='us-east-1')
+
+#Fetch and save a file from the internet
+def download_file(url, file_path):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        with open(file_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"File downloaded to {file_path}")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading: {e}")
+
+#Upload file to a bucket in S3
+bucket = 'ds2002-byz7ex'
+local_file = 'project/file'
+
+resp = s3.put_object(
+    Body = local_file,
+    Bucket = bucket,
+    Key = local_file
+)
+
+#Presign the file with an expiration time
+bucket_name = str
+object_name = str
+expires_in = 600
+
+response = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': bucket_name, 'Key': object_name},
+    ExpiresIn=expires_in
+)
+
+#Output the presigned URL
+print(response)
